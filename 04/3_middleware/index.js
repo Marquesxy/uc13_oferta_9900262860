794ff8a47d62169dc203e5f44420de9f8f323bee
RODOLFO GONCALVES DA SILVA <rodolfo.gsilva2@senacsp.edu.br>
2025-06-06 22:15:42 -0300

    aula 06*06

--- 04/3_middleware/index.js
+++ 04/3_middleware/index.js
@@ -2,13 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 const path = require('path')
-const basePath = path.join(__dirname, 'frontend')
+const basePath = path.join(__dirname, 'templates')
+
 
 const users = require('./users')
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-
+app.use(express.static("public"));
 
 var checkAuth = function (req, res, next) {
   req.authStatus = true
--- 04/3_middleware/users/index.js
+++ 04/3_middleware/users/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const path = require('path');
-const basePath = path.join(__dirname, '../frontend');
+const basePath = path.join(__dirname, '../templates');
 
 router.get('/add', (req, res) => {
   console.log('Carregando formulário de usuário')
