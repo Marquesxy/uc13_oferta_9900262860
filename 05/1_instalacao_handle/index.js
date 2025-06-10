210a690500afbf8f07e4037e93e5b0d22763651b
RODOLFO GONCALVES DA SILVA <rodolfo.gsilva2@senacsp.edu.br>
2025-06-09 21:50:41 -0300

    09-06

--- 05/1_instalacao_handle/index.js
+++ 05/1_instalacao_handle/index.js
@@ -2,7 +2,13 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 
 const app = express();
-app.engine('handlebars', exphbs.engine());
+
+const hbs = exphbs.create({
+  partialsDir: ["views/partials/"],
+});
+
+
+app.engine('handlebars', hbs.engine);
 
 app.set('view engine', 'handlebars');
 
@@ -11,12 +17,49 @@ app.get('/', function (req, res) {
     name: 'Rodolfo',
     surname: 'Gonçalves',
   }
-  
-  res.render('home', { user: user , auth: true })
+
+  res.render('home', { user: user , auth: true });
 });
 
 app.get("/dashboard", function (req, res) {
-  res.render("dashboard");
+  const items = ["Item a", "Item b", "Item c"];
+
+  res.render("dashboard", { items: items });
 });
+
+app.get("/post", function (req, res) {
+  const post = {
+    title: "Aprender Node.js",
+    category: "Node.js",
+    body: "Node.js é muito utilizado na programação hoje em dia",
+    comments: 4,
+  };
+  res.render("blogpost", { post });
+});
+
+app.get("/blog", function (req, res) {
+  const posts = [
+    {
+      title: "Aprender Node.js",
+      category: "Node.js",
+      body: "Node.js é muito utilizado na programação hoje em dia",
+      comments: 4,
+    },
+    {
+      title: "PHP ainda vale a pena?",
+      category: "PHP",
+      body: "",
+      comments: 12,
+    },
+    {
+      title: "Os segredos de JavaScript",
+      category: "JavaScript",
+      body: "",
+      comments: 5,
+    },
+  ];
+  res.render("blog", { posts });
+});
+
 
 app.listen(3000);
