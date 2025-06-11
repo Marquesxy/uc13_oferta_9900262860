7f2d74190fc8717d24d26b676de47a4d5c0ae30e
RODOLFO GONCALVES DA SILVA <rodolfo.gsilva2@senacsp.edu.br>
2025-06-11 19:37:50 -0300

    11-06

--- 06/index.js
+++ 06/index.js
@@ -12,6 +12,7 @@ app.use(
     extended: true,
   }),
 )
+
 app.use(express.json())
 
 app.use(express.static('public'))
@@ -42,11 +43,15 @@ app.get('/books', function (req, res) {
     if (err) {
       console.log(err)
     }
+
     const books = data
+
     console.log(data)
+
     res.render('books', { books })
   })
 })
+
 app.get('/books/:id', function (req, res) {
   const id = req.params.id
 
@@ -56,27 +61,76 @@ app.get('/books/:id', function (req, res) {
     if (err) {
       console.log(err)
     }
+
     const book = data[0]
+
     console.log(data[0])
+
     res.render('book', { book })
   })
 })
 
+app.get('/books/edit/:id', function (req, res) {
+  const id = req.params.id
 
+  const query = `SELECT * FROM books WHERE id = ${id}`
+
+  conn.query(query, function (err, data) {
+    if (err) {
+      console.log(err)
+    }
+
+    const book = data[0]
+
+    console.log(data[0])
+
+    res.render('editbook', { book })
+  })
+})
+
+app.post('/books/updatebook', function (req, res) {
+  const id = req.body.id
+  const title = req.body.title
+  const pageqty = req.body.pageqty
+
+  const query = `UPDATE books SET title = '${title}', pageqty = ${pageqty} WHERE id = ${id}`
+
+  conn.query(query, function (err) {
+    if (err) {
+      console.log(err)
+    }
+
+    res.redirect(`/books/edit/${id}`)
+  })
+})
+
+app.post('/books/remove/:id', function (req, res) {
+  const id = req.params.id
+
+  const query = `DELETE FROM books WHERE id = ${id}`
+
+  conn.query(query, function (err) {
+    if (err) {
+      console.log(err)
+    }
+
+    res.redirect(`/books`)
+  })
+})
 
 const conn = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '',
   database: 'nodemysql',
-});
+})
+
 conn.connect(function (err) {
   if (err) {
     console.log(err)
   }
+
   console.log('Conectado ao MySQL!')
+
   app.listen(3000)
-});
-
-
-// Iniciar - 6_realizando_update
+})
